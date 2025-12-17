1696884e80d7639141ab6cbba347ed2545d313ed
Hannes Hertach <hanneshertach@gmail.com>
2025-12-17 13:40:17 +0100

    add keymap for li

--- tools/quick-edit/quick-edit.js
+++ tools/quick-edit/quick-edit.js
@@ -6,6 +6,7 @@ import { loadPage } from '../../blog/scripts/scripts.js';
 import { getSchema } from 'https://main--da-live--adobe.aem.live/blocks/edit/prose/schema.js';
 import { EditorState, EditorView } from 'https://main--da-live--adobe.aem.live/deps/da-y-wrapper/dist/index.js';
 import { showToolbar, hideToolbar, setCurrentEditorView, updateToolbarState, handleToolbarKeydown, positionToolbar } from './toolbar.js';
+import { createSimpleKeymap } from './simple-keymap.js';
 
 const { loadStyle } = await import(`${LIBS}/utils/utils.js`);
 loadStyle('/tools/quick-edit/quick-edit.css');
@@ -247,6 +248,7 @@ function createProsemirrorEditor(cursorOffset, state, port1) {
   const editorState = EditorState.create({
     doc,
     schema,
+    plugins: [createSimpleKeymap()],
   });
 
   const editorParent = document.createElement('div');
